9845740795aac7dcb5a7df7c455dc73cbd33f8e5
avenmia <mavend2@gmail.com>
2020-11-10 21:11:11 -0600

    Adding auto download when program is running

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { PDFViewer} from '@react-pdf/renderer'
+import {PDFDownloadLink, PDFViewer} from '@react-pdf/renderer'
 import './App.css';
 import { Resume } from "./components/Resume";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <PDFDownloadLink document={<Resume />} fileName="resume.pdf">
+          {
+            ({ blob, url, loading, error }) => {
+              if(!loading)
+              {
+                var pdfURL = window.URL.createObjectURL(blob);
+                var tempLink = document.createElement('a');
+                tempLink.href = pdfURL;
+                tempLink.setAttribute('download', 'resume.pdf');
+                tempLink.click();
+              }
+              return (loading ? 'Loading document...' :  'Download pdf')
+            }
+          }
+        </PDFDownloadLink>
         <PDFViewer className="PDFViewer">
           <Resume />
         </PDFViewer>
+      </div>
     </div>
   );
 }
