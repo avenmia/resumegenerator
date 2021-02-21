b4ba5ab442d3b97bd8fe002ff1213bf3f8939c2c
avenmia <mavend2@gmail.com>
2021-02-20 19:43:16 -0600

    Vertical rhythm (#6)
    * updating styles to use vertical rhythm
    
    * Updating resumeData

--- src/components/styledcontent/Header.tsx
+++ src/components/styledcontent/Header.tsx
@@ -3,5 +3,5 @@ import { Text } from "@react-pdf/renderer";
 import { styles } from "../styles/styles"
 
 export const HeaderSection = (header: string) => (
-  <Text style={styles.h2}>{header}</Text>
+  <Text style={styles.h1}>{header}</Text>
 )
